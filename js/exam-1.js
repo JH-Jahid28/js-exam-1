b80e42b1534e52bc2f16cd8096adca6c96c3d1b0
Md.Jahid ul-Islam <145700501+JH-Jahid28@users.noreply.github.com>
2023-10-23 14:11:19 +0600

    upload

---
+++ js/exam-1.js
@@ -0,0 +1,137 @@
+
+// 1 no ans 
+
+// var name = 'jahidul';
+
+// 5 no ans 
+
+// var name = 'my name';
+
+// 3 no ans
+
+// data type  example
+// strig      name
+// intger     12,23,24,25
+// flat       1.2,3,4,5
+
+// 9 no ans
+
+// var x = 10;
+// var y = 36;
+// var z = x+y;
+// console.log(z);
+
+// 8 no ans
+
+// operend  = (1,2,3,4,5,6,7,8,9,0)
+
+// 10 no ans 
+// var x = 8;
+// var y = 375;
+// var remainder = (x/y);
+// console.log(remainder);
+
+// 13 no ans 
+
+// var x = 30;
+// var y = ('23');
+// var result = x+y;
+// console.log(result);
+
+// 12 no ans 
+
+// var number_of_subjects = 4;
+// var math = 75;
+// var chemistry = 60;
+// var physics = 70;
+// var bangla = 40;
+
+// var total_number = math + chemistry + physics + bangla;
+
+// var total_avg = total_number / number_of_subjects;
+
+// console.log(total_avg)
+
+
+// 4 no ans 
+
+// name diye suru hoy;
+// number diye variable suru hoy na;
+// keyboard k variable hishabe bebohar kora jabe na;
+
+// 20 no ans 
+
+// var team1 = ["rahim", "karim",];
+// var team2 = ["tanim", "biplob", "tareq",];
+// var team3 = ["sajjad", "arefin",];
+// var team_all = (team1.concat(team2,team3));
+// console.log(team_all);
+
+// 2 no ans 
+
+// 1st use keyboard var;
+// type variable name ;
+// typing variable name;
+// use = to defain variable Value;
+// use ; to complete the setment ;
+
+// 15 no ans 
+
+// var string = '120'
+// var string2number = parseInt(strig);
+
+// 14 no ans 
+
+// kuno type e ase seta dekar jonno typeof bebohar kora hoy ;
+
+// example : var number = '1,2,3,5';
+// console.log(typeof(number));
+
+
+// 16 no ans 
+//  Array index 0 theke suru hoy ;
+
+
+// 17 no ans 
+
+// var numbers = [34,56,74,24,88,34,776,34,5];
+// number [5]=334;
+// console.log(number); 
+
+// 18 no ans 
+
+// var number = [34,56,74,24,88,34,776,34,5];
+// number.push(234);
+// var number2 = number.unshift(998)
+// console.log(numbers);
+
+
+// 21 no ans 
+
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.shift(0);
+// console.log(fruits);
+
+// 22 no ans 
+
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(3, 0, "Lichi", "Pineapple", "Watermelon");
+// console.log(fruits);
+
+// 25 no ans 
+
+// var total_pen = 10;
+// var cost_pen = 185;
+// var buy_pen = 3;
+
+// var per_pen_cost = cost_pen / total_pen;
+// var buy_pen_cost = per_pen_cost * buy_pen;
+// console.log(buy_pen_cost);
+
+
+
+
+
+
+
+ 
